2e7a29a77c08cba74b54714a6508cf2b83034a67
Hafiz Shahman <hafizshahman1@gmail.com>
2024-11-06 01:38:27 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('user_type');
+            $table->string('user_type')->nullable();
             $table->integer('role_id')->nullable()->comment('3 = userone, 4 = usertwo');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
