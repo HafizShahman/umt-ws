e42d3416436604bae6e0e2cbb53325fa97d1c17f
HafizShahman <hafizshahman1@gmail.com>
2024-11-07 17:49:18 +0800

    u

--- storage/framework/views/a35779255f45e631b113f55027a9e7b956517c43.php
+++
@@ -1,6 +0,0 @@
-<div class="footer-wrapper">
-    <div class="footer-section f-section-1">
-        <p class="">Copyright © <?php echo date("Y");  ?> <a target="_blank"
-                href="https://revenuediscovery.com/">Revenue Discovery Sdn Bhd</a>, All rights reserved.</p>
-    </div> 
-</div><?php /**PATH C:\laragon\www\umt\resources\views/general/inc/footer.blade.php ENDPATH**/ ?>
